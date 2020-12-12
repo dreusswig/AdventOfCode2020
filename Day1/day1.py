44f9bad6fed86db49e8b75730498a14982b07108
dreusswig <dennis.reusswig@gmx.net>
2020-12-12 13:46:47 +0100

    Initial Commit

---
+++ Day1/day1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+input = np.loadtxt("Day1/input.txt", dtype=int)
+
+for i, num1 in enumerate(input):
+    done = 0
+    for num2 in input[i+1:len(input)-1]:
+        if num1 + num2 == 2020:
+            done = 1
+            break
+    if done == 1:
+        break
+
+result = num1*num2
+
+print(str(num1) + ' * ' + str(num2) + ' = ' + str(result))
